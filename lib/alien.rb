d31da704b1d5463c7cff2626ebaf5b8ba518bfec
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2020-07-30 20:20:25 +1200

    start playing with the main connection class

--- lib/alien.rb
+++ lib/alien.rb
@@ -5,31 +5,55 @@ module Alien
   class Error < StandardError; end
 
   class << self
-    def start(name:, **options)
-      Connection.new(name: name, **options)
+    def start(service_name:, **options)
+      @connection = Connection.new(service_name: service_name, **options)
+      
+      @connection.queue.subscribe do |delivery_info, metadata, payload|
+        puts delivery_info.inspect
+        puts metadata.inspect
+        puts "Received #{payload}"
+      end
     end
 
     def stop
+      @connection.server.stop
+    end
+
+    def publish(service_name, payload, &block)
+      @connection.exchange.publish(payload, routing_key: service_name)
     end
   end
 
   class Connection
 
-    attr_reader :ampq, :exchange, :name
+    attr_reader :server, :service_name
 
-    def initialize(name:, **options)
-      @name = name
-      @ampq = Bunny.new
-      @ampq.start
+    def initialize(service_name:, **options)
+      @service_name = service_name
+      @server = Bunny.new
+      #@channel = @server.create_channel
+      #@queue = @channel.queue(name, auto_delete: true)
+      #@exchange = @channel.default_exchange
+    end
 
-     # ch = @ampq.create_channel
-     # q = ch.queue(name, auto_delete: true)
-     # @exchange = ch.default_exchange
+    def start
+      server.start
+    end
 
-     # q.subscribe do |delivery_info, metadata, payload|
-     #   puts "Received #{payload}"
-     # end
+    def stop
+      server.stop
+    end
+
+    def channel
+      @channel ||= @server.create_channel
+    end
+
+    def queue
+      @queue ||= channel.queue(service_name, auto_delete: true)
+    end
+
+    def exchange
+      @exchange ||= channel.default_exchange
     end
   end
-
 end
--- spec/alien_connection_spec.rb
+++ spec/alien_connection_spec.rb
@@ -2,10 +2,26 @@ RSpec.describe Alien::Connection do
 
   subject { described_class }
 
-  it 'initializes a RabbitMQ connection' do
-    conn = subject.new(name: 'Test')
+  before do
+    #connection = double(Bunny)
+    #allow(Bunny).to receive(:new).and_return(connection)
+    #allow(connection).to receive(:start)
+    #allow(connection).to receive(:create_channel)
+    #allow(connection).to receive(:queue)
+  end
 
-    expect(conn.name).to eq('Test')
-    expect(conn.ampq).to be_a(Bunny::Session)
+  it 'initializes a RabbitMQ connection' do
+    expect(Bunny).to receive(:new)
+    conn = subject.new(service_name: 'Test')
+
+    expect(conn.service_name).to eq('Test')
+  end
+
+  it 'starts the network connection' do
+    bunny_session = instance_double(Bunny::Session, start: Object.new)
+    expect(Bunny).to receive(:new).and_return(bunny_session)
+    conn = subject.new(service_name: 'Test')
+
+    expect(conn.start).to be_a(Object)
   end
 end
